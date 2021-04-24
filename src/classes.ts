7ba7d4d5229cc312dd6247cb3061b6949fbc58a0
steelvelveteen <joeyvico@gmail.com>
2021-04-25 09:01:18 +0930

    Classes and inheritance refresh

---
+++ src/classes.ts
@@ -0,0 +1,22 @@
+interface IPerson {
+    name: string;
+    age: number;
+    speak: () => void;
+    /** Non arrow function */
+    // speak2(): void;
+}
+
+class Person implements IPerson {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    speak = () => console.log('Name: ' + this.name + ', Age: ' + this.age);
+}
+
+let p1 = new Person('Joey', 47);
+p1.speak();
