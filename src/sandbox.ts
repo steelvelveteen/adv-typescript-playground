0ddd174afa7a6d1ec21b0bbe650cee500381a696
steelvelveteen <joeyvico@gmail.com>
2021-04-26 17:48:38 +0930

    Added a sandbox file

---
+++ src/sandbox.ts
@@ -0,0 +1,25 @@
+interface Member {
+    name: string;
+    age: number;
+    address: string;
+    notification: boolean;
+    email: string;
+}
+
+const person: Member = {
+    name: 'Joey',
+    age: 47,
+    address: '82 jeffcott street',
+    notification: false,
+    email: 'joeyvico@gmail.com',
+};
+const updatedperson = {
+    name: 'Joey Vico',
+    address: '82 Jeffcott St',
+    notification: true,
+};
+
+console.log(person);
+console.log(updatedperson);
+const updated = { ...person, ...updatedperson };
+console.log(updated);
