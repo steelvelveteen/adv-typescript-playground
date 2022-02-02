29208b2c4483e3cfde08b8efdaed419afc214ed8
joeyOnMac <joeyvico@gmail.com>
2022-02-02 20:55:23 +0100

    Normalzied format and linting

--- src/generics-types.ts
+++ src/generics-types.ts
@@ -7,15 +7,15 @@ console.log(ident);
 
 const genericIdentity = <T>(arg: T): T => {
     return arg;
-}
+};
 
 let genIdent = genericIdentity<number>(55);
 console.log(genIdent);
 
 const genArrayIdentity = <T>(arg: T[]): T[] => {
     return arg;
-}
-let argument = ["one", "two", "three"];
+};
+let argument = ['one', 'two', 'three'];
 let genArrayIdent = genArrayIdentity<string>(argument);
 console.log(genArrayIdent.length);
 
