5afd5de33f329ca8efd11fb0075cf7e7046de6e0
joeyOnMac <joeyvico@gmail.com>
2022-02-10 15:49:42 +0100

    Normalizing coding style to es 6 arrow functions

--- src/generics-types.ts
+++ src/generics-types.ts
@@ -1,20 +1,15 @@
-function identity(arg: number) {
-  return arg;
-}
+const identity = (arg: number) => arg;
 
 const ident = identity(45);
 console.log(ident);
 
-const genericIdentity = <T>(arg: T): T => {
-  return arg;
-};
+const genericIdentity = <T>(arg: T): T => arg;
 
 const genIdent = genericIdentity<number>(55);
 console.log(genIdent);
 
-const genArrayIdentity = <T>(arg: T[]): T[] => {
-  return arg;
-};
+const genArrayIdentity = <T>(arg: T[]): T[] => arg;
+
 const argument = ['one', 'two', 'three'];
 const genArrayIdent = genArrayIdentity<string>(argument);
 console.log(genArrayIdent.length);
