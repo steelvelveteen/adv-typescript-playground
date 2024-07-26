a624ff11c54ffd987cd16c169d4baf2a4b011a39
JoseVico <jose.vico@evorait.com>
2024-07-26 09:03:10 +0200

    Formatting with new configuration

--- src/classes.ts
+++ src/classes.ts
@@ -1,48 +1,48 @@
 interface IPerson {
-  firstName: string;
-  age: number;
-  speak: () => void;
-  /** Non arrow function */
-  // speak2(): void;
+    firstName: string;
+    age: number;
+    speak: () => void;
+    /** Non arrow function */
+    // speak2(): void;
 }
 
 class Person implements IPerson {
-  firstName: string;
-  age: number;
+    firstName: string;
+    age: number;
 
-  constructor(firstName: string, age: number) {
-    this.firstName = firstName;
-    this.age = age;
-  }
+    constructor(firstName: string, age: number) {
+        this.firstName = firstName;
+        this.age = age;
+    }
 
-  speak = () => console.log('Person Info: ' + this.firstName + ', Age: ' + this.age);
+    speak = () => console.log('Person Info: ' + this.firstName + ', Age: ' + this.age);
 }
 
 const p1 = new Person('Joey', 47);
 p1.speak();
 
 class Employee extends Person {
-  employeeType: string;
-  salary: number;
+    employeeType: string;
+    salary: number;
 
-  constructor(firstName: string, age: number, employeeType: string, salary: number) {
-    super(firstName, age);
+    constructor(firstName: string, age: number, employeeType: string, salary: number) {
+        super(firstName, age);
 
-    this.employeeType = employeeType;
-    this.salary = salary;
-  }
+        this.employeeType = employeeType;
+        this.salary = salary;
+    }
 
-  speak = () =>
-    console.log(
-      'Employee Info: ' +
-        this.firstName +
-        ', Age: ' +
-        this.age +
-        ', Type: ' +
-        this.employeeType +
-        ', Salary: ' +
-        this.salary
-    );
+    speak = () =>
+        console.log(
+            'Employee Info: ' +
+                this.firstName +
+                ', Age: ' +
+                this.age +
+                ', Type: ' +
+                this.employeeType +
+                ', Salary: ' +
+                this.salary
+        );
 }
 
 const employee1 = new Employee('Maria Elizabeth Winstead', 39, 'Actress', 45000);
