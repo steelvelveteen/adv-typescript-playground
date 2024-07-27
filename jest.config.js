047241518e98194915c4e51e4ba6cf7d9b7a695f
joeyOnMac <joeyvico@gmail.com>
2024-07-27 21:31:30 +0200

    Added unit test to the project

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// module.exports = {
+//   testEnvironment: "node",
+//   transform: {
+//     "^.+.tsx?$": ["ts-jest",{}],
+//   },
+// };
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'js']
+};
